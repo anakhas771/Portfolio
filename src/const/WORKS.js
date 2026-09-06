674ebecdd326d51988b81b347286606433f53390
Anakha S <anakhas771@gmail.com>
2026-09-07 00:11:21 +0530

    update project information

--- src/const/WORKS.js
+++ src/const/WORKS.js
@@ -1,54 +1,33 @@
 export default [
   {
-    key: 'sketch-threejs',
-    title: 'Sketch of Three.js',
-    shortTitle: 'Sketch of Three.js',
+    key: 'AI-Incident-Response-Platform',
+    title: 'AI-Incident-Response-Platform ',
+    shortTitle: 'AI-Incident-Response-Platform ',
     description:
-      'Interactive sketches made with three.js. I start to create the sketches from 2015 and continue even now.',
+      'An AI-powered incident response platform that helps engineering teams investigate production incidents using AI-assisted analysis, Retrieval-Augmented Generation (RAG), semantic knowledge retrieval, and intelligent remediation workflows.',
     credit: 'Developer : Anakha S',
-    href: 'https://ykob.github.io/sketch-threejs/',
+    href: 'https://github.com/anakhas771/AI-Incident-Response-Platform',
     textWidth: 1260
   },
   {
-    key: 'warpdrive',
-    title: 'WarpDrive',
-    shortTitle: 'WarpDrive',
+    key: 'MotionForge',
+    title: 'MotionForge',
+    shortTitle: 'MotionForge',
     description:
-      "The Chrome Extension, a data visualization of user's activity on a chrome browser. I was responsible for a content named REQUEST VIEWER and devised how to mix 2D and 3D user interfaces.",
-    credit:
-      'Client : KDDI CORPORATION, NICT\nAgency : THE STRIPPERS Co.\nDeveloper : Anakha S',
-    href: 'https://warpdrive-project.jp/',
+      'MotionForge is a modern animation library built with Next.js, TypeScript, Tailwind CSS, Framer Motion, and GSAP. It provides a growing collection of UI animation previews organized into categories. Users can browse animations, preview them directly in the browser, and download available source packages as ZIP files.',
+    credit: 'Developer : Anakha S',
+    href: 'https://motionforge-roan.vercel.app/',
     textWidth: 700
   },
+
   {
-    key: 'hassyadai',
-    title: 'HASSYADAI Inc.',
-    shortTitle: 'HASSYADAI Inc.',
+    key: 'KnowledgeOS',
+    title: 'KnowledgeOS',
+    shortTitle: 'KnowledgeOS',
     description:
-      'The website of the temp agency in Japan made with various WebGL effects. Also, I got CSSDA WOTD for the first time by making this site.',
-    credit:
-      'Client : HASSYADAI Inc.\nAgency : BEES/HONEY INC\nDeveloper : Anakha S',
-    href: 'https://hassyadai.com',
-    textWidth: 940
-  },
-  {
-    key: 'imago',
-    title: 'IMAGO',
-    shortTitle: 'IMAGO',
-    description:
-      'The work of the paint artist Hiroshi Yoshida. I expressed it as a 3D world on the web.',
-    credit: 'Client : Hiroshi Yoshida\nDeveloper : Anakha S',
-    href: 'http://hiroshi-yoshida.noor.jp/imago/',
-    textWidth: 390
-  },
-  {
-    key: 'best-film-2018',
-    title: 'Filmarks Best Film of 2018',
-    shortTitle: 'Best Film of 2018',
-    description:
-      'The special site launched at the end of the year of 2018 by Japanese film review service Filmarks.\n* This site is already closed.',
-    credit: 'Client : TSUMIKI INC.\nDeveloper : Anakha S',
-    href: 'https://thefwa.com/cases/filmarks-best-film-of-2018-p2',
+      'KnowledgeOS is an enterprise-grade AI knowledge intelligence platform that converts scattered company information into a centralized, searchable, and continuously improving organizational knowledge system.',
+    credit: 'Developer : Anakha S',
+    href: 'https://github.com/anakhas771/KnowledgeOS',
     textWidth: 1720
   }
 ];
