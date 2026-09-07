1e85ac62860a76ea89c41757712dda9fe4849bf1
Anakha S <anakhas771@gmail.com>
2026-09-07 11:10:13 +0530

    fix project thumbnail paths

--- src/webgl/index.js
+++ src/webgl/index.js
@@ -143,9 +143,9 @@ export default class WebGLContent {
 
       // Project thumbnails
       require(`@/assets/img/webgl/thumb_blank.${webpExe}`),
-      require(`@/assets/img/webgl/thumb_ai_incident_response.${webpExe}`),
-      require(`@/assets/img/webgl/thumb_motionforge.${webpExe}`),
-      require(`@/assets/img/webgl/thumb_knowledgeos.${webpExe}`)
+      require('@/assets/img/webgl/thumb_ai_incident_response.png'),
+      require('@/assets/img/webgl/thumb_motionforge.png'),
+      require('@/assets/img/webgl/thumb_knowledgeos.png')
     ];
     store.commit('setPreloadMax', assetsObj.length + assetsImgs.length);
 
