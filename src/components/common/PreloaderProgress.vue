8c957dbb73288353d0a81574d75063c029b99615
Anakha S <anakhas771@gmail.com>
2026-09-07 15:36:06 +0530

    fixed preloader round circle  size

--- src/components/common/PreloaderProgress.vue
+++ src/components/common/PreloaderProgress.vue
@@ -112,7 +112,7 @@ export default {
             href="#text-circle-path"
             xlink:href="#text-circle-path"
           >
-            ANAKHA • DEVELOPER • ANAKHA • DESIGNER • ANAKHA • ENGINEER •
+            ANAKHA • DEVELOPER • ANAKHA • DESIGNER • ANAKHA • ENGINEER • STUDIO •
           </textPath>
         </text>
 
