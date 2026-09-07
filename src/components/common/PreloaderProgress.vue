845b69f7725c5f4994680fb7106a8dac63937dd3
Anakha S <anakhas771@gmail.com>
2026-09-07 19:07:38 +0530

    fix: customize portfolio preloader

--- src/components/common/PreloaderProgress.vue
+++ src/components/common/PreloaderProgress.vue
@@ -1,30 +1,21 @@
 <script>
 export default {
   name: 'PreloaderProgress',
-
   computed: {
     ratio() {
       const { preloadProgress, preloadMax } = this.$store.state;
-
-      if (!preloadMax) {
-        return 0;
-      }
-
-      return Math.min(preloadProgress / preloadMax, 1);
+      return preloadProgress / preloadMax;
     },
-
     stylesRightRect() {
       return {
         opacity: this.ratio >= 0.5 ? 1 : 0
       };
     },
-
     stylesLeftRect() {
       return {
         opacity: this.ratio >= 0.5 ? 0 : 1
       };
     },
-
     stylesRotateRect() {
       return {
         transform: `rotate(${this.ratio * 354}deg)`
@@ -35,155 +26,78 @@ export default {
 </script>
 
 <template lang="pug">
-.preloader-progress
-  .preloader-progress__inner.
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-      width="252"
-      height="252"
-      viewBox="0 0 252 252"
-    >
-      <defs>
-        <path
-          id="text-circle-path"
-          d="M 126,126 m -98,0 a 98,98 0 1,1 196,0 a 98,98 0 1,1 -196,0"
-        />
-      </defs>
-
-      <!-- Circular progress -->
-      <g mask="url(#mask-text)">
-        <mask id="mask-rotate">
-          <g class="mask-rotate-group">
-
-            <path
-              class="mask-rotate-rect"
-              :style="stylesRotateRect"
-              fill="#ffffff"
-              d="M0 0h126v252H0z"
-            />
-
-            <path
-              :style="stylesLeftRect"
-              d="M0 0h126v252H0z"
-            />
-
-            <path
-              :style="stylesRightRect"
-              fill="#ffffff"
-              d="M126 0h126v252H126z"
-            />
-
-          </g>
+  .preloader-progress
+    .preloader-progress__inner.
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="252" height="252" viewBox="0 0 252 252">
+        <defs>
+          <path id="text-circle-path" d="M 126, 126 m -98, 0 a 98,98 0 1,1 196,0 a 98,98 0 1,1 -196,0"/>
+        </defs>
+        <g mask="url(#mask-text)">
+          <mask id="mask-rotate">
+            <g class="mask-rotate-group">
+              <path class="mask-rotate-rect" :style="stylesRotateRect" fill="#ffffff" d="M0 0h126v252H0z"/>
+              <path :style="stylesLeftRect" d="M0 0h126v252H0z"/>
+              <path :style="stylesRightRect" fill="#ffffff" d="M126 0h126v252H126z"/>
+            </g>
+          </mask>
+          <path fill="#564e45" d="M0 0h252v252H0z"/>
+          <path fill="#dcc5a2" mask="url(#mask-rotate)" d="M0 0h252v252H0z"/>
+        </g>
+        <mask id="mask-text">
+          <path fill="#000000" d="M0 0h252v252H0z"/>
+          <text fill="#ffffff" font-size="10.5" font-weight="700" letter-spacing="1.8" font-family="-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif">
+            <textPath href="#text-circle-path" xlink:href="#text-circle-path">
+              ANAKHA • DEVELOPER • ANAKHA • DESIGNER • ANAKHA • ENGINEER • ANAKHA • CREATOR • ANAKHA 
+            </textPath>
+          </text>
         </mask>
-
-        <!-- Base color -->
-        <path
-          fill="#564e45"
-          d="M0 0h252v252H0z"
-        />
-
-        <!-- Progress color -->
-        <path
-          fill="#dcc5a2"
-          mask="url(#mask-rotate)"
-          d="M0 0h252v252H0z"
-        />
-      </g>
-
-      <!-- Circular typography mask -->
-      <mask id="mask-text">
-
-        <!-- Hide everything -->
-        <path
-          fill="#000000"
-          d="M0 0h252v252H0z"
-        />
-
-        <!-- Circular text -->
-        <text
-          fill="#ffffff"
-          font-size="10.5"
-          font-weight="700"
-          letter-spacing="1.8"
-          font-family="-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif"
-        >
-          <textPath
-            href="#text-circle-path"
-            xlink:href="#text-circle-path"
-          >
-            ANAKHA • DEVELOPER • ANAKHA • DESIGNER • ANAKHA • ENGINEER • STUDIO • CREATIVE •
-          </textPath>
-        </text>
-
-      </mask>
-    </svg>
+      </svg>
 </template>
 
 <style lang="scss">
 .preloader-progress {
   position: absolute;
-
   animation-name: rotate;
   animation-duration: 20s;
   animation-timing-function: linear;
   animation-iteration-count: infinite;
-
   @include l-more-than-mobile {
     width: 252px;
     height: 252px;
     top: calc(50% - 126px);
     left: calc(50% - 126px);
   }
-
   @include l-mobile {
     width: 150px;
     height: 150px;
     top: calc(50% - 75px);
     left: calc(50% - 75px);
   }
-
   &__inner {
-    width: 100%;
-    height: 100%;
-
     //
-    // Enter animation
-    // ==============
+    // transition
+    // ==========
     .preloader-enter & {
       opacity: 0;
       transform: scale(0.6);
     }
-
     .preloader-enter-to & {
-      opacity: 1;
       transform: scale(1);
-
       transition-duration: 1.4s;
       transition-timing-function: $easeOutCirc;
       transition-property: opacity, transform;
     }
-
-    //
-    // Leave animation
-    // =============
     .preloader-leave-to & {
       opacity: 0;
       transform: scale(1.8);
-
       transition-duration: 1.4s;
       transition-delay: 0.8s;
       transition-timing-function: $easeInExpo;
       transition-property: opacity, transform;
     }
   }
-
   svg {
     display: block;
-
-    width: 100%;
-    height: 100%;
-
     backface-visibility: hidden;
     transform: translate3d(0, 0, 0);
 
@@ -191,21 +105,15 @@ export default {
       width: 252px;
       height: 252px;
     }
-
     @include l-mobile {
       width: 150px;
       height: 150px;
     }
   }
-
-  //
-  // Progress rotation
-  // =================
   .mask-rotate-group {
     transform: rotate(34deg);
     transform-origin: center center;
   }
-
   .mask-rotate-rect {
     transform-origin: center center;
   }
