6a3e85d623736f3903000b9c6afff0a2e4cec677
Anakha S <anakhas771@gmail.com>
2026-09-07 15:12:41 +0530

    fixed preloader round size

--- src/components/common/PreloaderProgress.vue
+++ src/components/common/PreloaderProgress.vue
@@ -1,92 +1,123 @@
 <script>
 export default {
   name: 'PreloaderProgress',
+
   computed: {
     ratio() {
       const { preloadProgress, preloadMax } = this.$store.state;
-      return preloadProgress / preloadMax;
+
+      if (!preloadMax) {
+        return 0;
+      }
+
+      return Math.min(1, Math.max(0, preloadProgress / preloadMax));
     },
-    stylesRightRect() {
-      return {
-        opacity: this.ratio >= 0.5 ? 1 : 0
-      };
-    },
-    stylesLeftRect() {
-      return {
-        opacity: this.ratio >= 0.5 ? 0 : 1
-      };
-    },
-    stylesRotateRect() {
-      return {
-        transform: `rotate(${this.ratio * 354}deg)`
-      };
+
+    progressDashOffset() {
+      const circumference = 2 * Math.PI * 98;
+
+      return circumference * (1 - this.ratio);
     }
   }
 };
 </script>
 
 <template lang="pug">
-  .preloader-progress
-    .preloader-progress__inner.
-      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="252" height="252" viewBox="0 0 252 252">
-        <defs>
-          <path id="text-circle-path" d="M 126, 126 m -98, 0 a 98,98 0 1,1 196,0 a 98,98 0 1,1 -196,0"/>
-        </defs>
-        <g mask="url(#mask-text)">
-          <mask id="mask-rotate">
-            <g class="mask-rotate-group">
-              <path class="mask-rotate-rect" :style="stylesRotateRect" fill="#ffffff" d="M0 0h126v252H0z"/>
-              <path :style="stylesLeftRect" d="M0 0h126v252H0z"/>
-              <path :style="stylesRightRect" fill="#ffffff" d="M126 0h126v252H126z"/>
-            </g>
-          </mask>
-          <path fill="#564e45" d="M0 0h252v252H0z"/>
-          <path fill="#dcc5a2" mask="url(#mask-rotate)" d="M0 0h252v252H0z"/>
-        </g>
-        <mask id="mask-text">
-          <path fill="#000000" d="M0 0h252v252H0z"/>
-          <text fill="#ffffff" font-size="10.5" font-weight="700" letter-spacing="1.8" font-family="-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif">
-            <textPath href="#text-circle-path" xlink:href="#text-circle-path">
-              ANAKHA • DEVELOPER • ANAKHA • DESIGNER • ANAKHA • ENGINEER 
-            </textPath>
-          </text>
-        </mask>
-      </svg>
+.preloader-progress
+  .preloader-progress__inner
+    svg(
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      width="252"
+      height="252"
+      viewBox="0 0 252 252"
+    )
+      defs
+        path#text-circle-path(
+          d="M 126,126 m -98,0 a 98,98 0 1,1 196,0 a 98,98 0 1,1 -196,0"
+        )
+
+      //- Background circle
+      circle(
+        cx="126"
+        cy="126"
+        r="98"
+        fill="none"
+        stroke="#564e45"
+        stroke-width="4"
+      )
+
+      //- Loading progress circle
+      circle(
+        cx="126"
+        cy="126"
+        r="98"
+        fill="none"
+        stroke="#dcc5a2"
+        stroke-width="4"
+        stroke-linecap="butt"
+        :stroke-dasharray="2 * Math.PI * 98"
+        :stroke-dashoffset="progressDashOffset"
+        transform="rotate(-90 126 126)"
+      )
+
+      //- Complete circular text
+      text(
+        fill="#ffffff"
+        font-size="10.5"
+        font-weight="700"
+        letter-spacing="1.8"
+        font-family="-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif"
+      )
+        textPath(
+          href="#text-circle-path"
+          xlink:href="#text-circle-path"
+        )
+          | ANAKHA • DEVELOPER • ANAKHA • DESIGNER • ANAKHA • ENGINEER
 </template>
 
 <style lang="scss">
 .preloader-progress {
   position: absolute;
+
   animation-name: rotate;
   animation-duration: 20s;
   animation-timing-function: linear;
   animation-iteration-count: infinite;
+
   @include l-more-than-mobile {
     width: 190px;
     height: 190px;
     top: calc(50% - 95px);
     left: calc(50% - 95px);
   }
+
   @include l-mobile {
     width: 120px;
     height: 120px;
     top: calc(50% - 60px);
     left: calc(50% - 60px);
   }
+
   &__inner {
-    //
-    // transition
-    // ==========
+    width: 100%;
+    height: 100%;
+
+    // Enter animation
     .preloader-enter & {
       opacity: 0;
       transform: scale(0.6);
     }
+
     .preloader-enter-to & {
+      opacity: 1;
       transform: scale(1);
       transition-duration: 1.4s;
       transition-timing-function: $easeOutCirc;
       transition-property: opacity, transform;
     }
+
+    // Leave animation
     .preloader-leave-to & {
       opacity: 0;
       transform: scale(1.8);
@@ -96,21 +127,18 @@ export default {
       transition-property: opacity, transform;
     }
   }
+
   svg {
     display: block;
     width: 100%;
     height: 100%;
+    overflow: visible;
     backface-visibility: hidden;
     transform: translate3d(0, 0, 0);
   }
 
-  .mask-rotate-group {
-    transform: rotate(34deg);
-    transform-origin: center center;
-  }
-
-  .mask-rotate-rect {
-    transform-origin: center center;
+  text {
+    dominant-baseline: auto;
   }
 }
 </style>
